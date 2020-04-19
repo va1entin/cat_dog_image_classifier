6dbdcb3776f1aa9c46fa014579235bac6e051b40
va1entin <valh@mailbox.org>
2020-04-19 17:44:19 +0200

    init

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import *
+from process_image import process_image
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def upload():
+	return render_template("index.html")
+
+@app.route('/upload', methods=['POST'])
+def success():
+	if request.method == 'POST':
+		f = request.files['file']
+		#print(type(f))
+		#print(dir(f))
+		image_file_name = 'working_dir/' + f.filename
+		f.save(image_file_name)
+		try:
+			prediction = process_image(image_file_name)
+		except:
+			prediction = "Prediction failed"
+		finally:
+			delete_image_file(image_file_name)
+		return render_template("index.html", result=prediction)
+
+def delete_image_file(image_file_name):
+	if image_file_name.startswith('working_dir/'):
+		os.remove(image_file_name)
+
+if __name__ == '__main__':
+	app.run(debug=True)
---
+++ process_image.py
@@ -0,0 +1,27 @@
+import cv2
+import keras
+import numpy as np
+
+nrows = 150
+ncolumns = 150
+
+path = "ml/model_keras.h5"
+model = keras.models.load_model(path)
+model._make_predict_function()
+
+def preprocess_image(image_file):
+    read_image = cv2.resize(cv2.imread(image_file, cv2.IMREAD_COLOR), (nrows,ncolumns), interpolation=cv2.INTER_CUBIC)
+    preprocessed_image = np.expand_dims(read_image, axis=0)
+    return preprocessed_image
+
+def predict(model, preprocessed_image):
+    numeric_prediction = model.predict(preprocessed_image)
+    if numeric_prediction > 0.5:
+        return "dog"
+    else:
+        return "cat"
+
+def process_image(image_file_name):
+    preprocessed_image = preprocess_image(image_file_name)
+    prediction = predict(model, preprocessed_image)
+    return prediction
